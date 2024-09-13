5062e2113e2174a2d9199da5f958e22ddcd2f277
ASL <145647432+ASLplay@users.noreply.github.com>
2024-09-13 12:57:58 +0300

    path

--- assets/js/functions/application.js
+++ assets/js/functions/application.js
@@ -1,11 +1,22 @@
-let divApplication = document.getElementsByClassName("application")[0];
+let array = ['Landing Page', 'SPA', 'Multi Page'];
 
-function openApplication() {
-    divApplication.style.display = "flex";
-    setTimeout(() => {
-        divApplication.classList.remove("close");
-    }, 200)
-}
+let divApplication = document.getElementsByClassName("application")[0];
+let h2 = divApplication.getElementsByTagName('h2')[0];
+
+let serviceBlock = document.getElementsByClassName('serviceBlock')[0].querySelectorAll("button");
+
+serviceBlock.forEach((el, i) => {
+    el.addEventListener('click', (e) => {
+
+        // h2.textContent = `Закзать ${el.parentNode.getElementsByTagName('h4')[0].textContent}` //Второй вариант
+        h2.textContent = `Закзать ${array[i]}`;
+
+        divApplication.style.display = "flex";
+        setTimeout(() => {
+            divApplication.classList.remove("close");
+        }, 200);
+    });
+});
 
 function closeApplication() {
     setTimeout(() => {
